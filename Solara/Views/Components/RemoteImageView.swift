4732d83ea72351b3a9bebfdddab7548b69a58040
akudamatata <583380790@qq.com>
2025-12-29 23:41:01 +0800

    tt

--- Solara/Views/Components/RemoteImageView.swift
+++ Solara/Views/Components/RemoteImageView.swift
@@ -10,31 +10,36 @@ struct RemoteImageView: View {
     @State private var isLoading = false
 
     var body: some View {
-        ZStack {
-            if let displayImage = image ?? placeholderImage {
-                Image(uiImage: displayImage)
-                    .resizable()
-                    .aspectRatio(contentMode: contentMode)
-            } else if isLoading {
-                ProgressView()
-            } else {
-                // Default Placeholder
-                ZStack {
-                    Color(red: 0.15, green: 0.15, blue: 0.16)
-                    Image(systemName: "music.note")
+        GeometryReader { geometry in
+            ZStack {
+                if let displayImage = image ?? placeholderImage {
+                    Image(uiImage: displayImage)
                         .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .font(.system(size: 40))
-                        .foregroundStyle(.white.opacity(0.2))
+                        .aspectRatio(contentMode: contentMode)
+                        .frame(width: geometry.size.width, height: geometry.size.height)
+                } else if isLoading {
+                    ProgressView()
+                        .frame(width: geometry.size.width, height: geometry.size.height)
+                } else {
+                    // Default Placeholder
+                    ZStack {
+                        Color(red: 0.15, green: 0.15, blue: 0.16)
+                        Image(systemName: "music.note")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: geometry.size.width * 0.4, height: geometry.size.height * 0.4)
+                            .foregroundStyle(.white.opacity(0.2))
+                    }
+                    .frame(width: geometry.size.width, height: geometry.size.height)
                 }
             }
+            .frame(width: geometry.size.width, height: geometry.size.height)
         }
         .task(id: url) { 
             image = nil // Reset when URL changes
             await loadImage() 
         }
         .animation(.easeInOut, value: image != nil)
-        .clipShape(Rectangle())
     }
 
     private func loadImage() async {
